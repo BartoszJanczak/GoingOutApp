0f65051f09b1fcd158dd5105282aae8785431546
Maciej Puchalski <maciejpe34@gmail.com>
2023-10-23 09:07:04 +0200

    Adds SQLite nugget, creates DataBase service and interface

---
+++ GoingOutApp/Services/IDataBaseService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoingOutApp.Services
+{
+    public interface IDataBaseService
+    {
+        void InitializeDatabase();
+    }
+}
