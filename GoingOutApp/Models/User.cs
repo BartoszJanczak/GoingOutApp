c244d72baddcf41e148e5adcd9a1f293a8304498
Maciej Puchalski <maciejpe34@gmail.com>
2023-11-02 22:14:27 +0100

    Adds database and models

---
+++ GoingOutApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoingOutApp.Models
+{
+    public class User
+    {   
+        [Key]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
