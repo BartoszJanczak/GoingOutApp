1fc7735fe7b371178d574e47d92825dd9008d2ef
Bartosz Janczak <bartek.janczak01@gmail.com>
2023-11-03 14:45:22 +0100

    Improve registration and login window appearance and add functionality to retrieve data from the fields

---
+++ GoingOutApp/LoginWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GoingOutApp
+{
+    /// <summary>
+    /// Interaction logic for ProfileWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private static RegisterWindow? _registerWindowInstance;
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        public class LoginUserData
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUser.Text;
+            //string password = PasswordBox.Password;
+            txtUser.Text = "Dupa";
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_registerWindowInstance == null)
+            {
+                _registerWindowInstance = new RegisterWindow();
+                _registerWindowInstance.Closed += (s, e) => _registerWindowInstance = null; // Reset _loginWindowInstance when the window is closed.
+                _registerWindowInstance.Show();
+            }
+            else
+            {
+                _registerWindowInstance.Focus(); // Skoncentruj się na istniejącym oknie profilu.
+            }
+        }
+        
+        private void textUser_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtUser.Focus();
+        }
+
+        private void txtUser_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtUser.Text) && txtUser.Text.Length > 0) 
+            {
+                textUser.Visibility = Visibility.Collapsed;
+            }
+            else 
+            { 
+                textUser.Visibility = Visibility.Visible;
+            }
+        }
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPassword.Focus();
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPassword.Password) && txtPassword.Password.Length > 0)
+            {
+                textPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPassword.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
--- GoingOutApp/RegisterWindow.xaml.cs
+++ GoingOutApp/RegisterWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -24,6 +26,16 @@ namespace GoingOutApp
             InitializeComponent();
         }
 
+        public class RegisterUserData
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Age { get; set; }
+            public string Gender { get; set; }
+        }
+
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
@@ -34,9 +46,33 @@ namespace GoingOutApp
             this.WindowState = WindowState.Minimized;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
         {
+            //UserData userData = new UserData()
+            //{
+            //    Username = UsernameTextBox.Text,
+            //    Password = PasswordTextBox.Text,
+            //    Name = NameTextBox.Text,
+            //    Surname = SurnameTextBox.Text,
+            //    Age = AgeTextBox.Text,
+            //    Gender = MaleRadioButton.IsChecked == true ? "Male" : "Female"
+            //};
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+            string name = NameTextBox.Text;
+            string surname = SurnameTextBox.Text;
+            string age = AgeTextBox.Text;
+            string gender = MaleRadioButton.IsChecked == true ? "Male" : "Female";
+            UsernameTextBox.Text = "dupa";
+        }
+        private void AgeTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
 
+        private bool IsTextAllowed(string text)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(text, "[0-9]");
         }
     }
 }
