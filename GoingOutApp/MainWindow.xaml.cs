30822b8ed5bbc33efb845cb31cc54377bf5622af
Markson <88060437+Markson-bit@users.noreply.github.com>
2023-11-16 22:52:49 +0100

    Wstępny zapis wydarzen i ich odczyt. Wyglad do zmiany, ale mozna juz pracowac nad wyswietlaniem szczegolow wydarzenia i integracja z mapa.

--- GoingOutApp/MainWindow.xaml.cs
+++ GoingOutApp/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using GoingOutApp.Services;
+﻿using GoingOutApp.Models;
+using GoingOutApp.Services;
 using System;
+using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 
@@ -16,10 +19,14 @@ namespace GoingOutApp
         private static LoginWindow ?_profileWindowInstance;
         private static AddTaskwindow? _addWindowInstance;
         private static UserProfileWindow? _userProfileWindowInstance;
+        private DataContext _database { get; set; } = new DataContext();
+
+        List<Event> events = new List<Event>(); 
 
         public MainWindow()
         {
             InitializeComponent();
+            OnShown();
 
             try
             {
@@ -41,6 +48,26 @@ namespace GoingOutApp
             }
         }
 
+        public void OnShown()
+        {
+            events = _database.GetEvents();
+
+            // Zakładając, że masz kontrolkę ListView o nazwie listViewEvents w MainWindow
+            ListOfEvents.Items.Clear(); // Wyczyść istniejące elementy
+
+            foreach (Event ev in events)
+            {
+                // Dodaj wydarzenie do ListView
+                ListOfEvents.Items.Add(new
+                {
+                    EventName = ev.EventName,
+                    //EventLocation = ev.EventLocation,
+                    EventDateTime = ev.EventDateTime.ToString(),
+                    // Reszta propercji
+                });
+            }
+        }
+
         private void Window_mousedown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
@@ -49,7 +76,17 @@ namespace GoingOutApp
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
-            Close();
+
+            // ZMAYKANIE WSZYSTKICH OKIEN TRZEBA ZROBIC BO JAK OTWORZYSZ JAKIES INNE I POTEM WSZYSTKO ZAMKNIESZ TO SIE PROGRAM NIE KONCZY IDK
+            this.Close();
+            //_profileWindowInstance.Close();
+            if(_addWindowInstance != null)
+                _addWindowInstance.Close();
+
+            if (_profileWindowInstance != null)
+                _profileWindowInstance.Close();
+            //_userProfileWindowInstance.Close();
+
         }
 
         private void MinimizeButton_Click(object sender, RoutedEventArgs e)
@@ -85,6 +122,8 @@ namespace GoingOutApp
             if (_addWindowInstance == null)
             {
                 _addWindowInstance = new AddTaskwindow();
+                _addWindowInstance.Owner = this;
+                _addWindowInstance.EventAdded += AddEventWindow_EventAdded; 
                 _addWindowInstance.Closed += (s, e) => _addWindowInstance = null; // Reset _profileWindowInstance when the window is closed.
                 _addWindowInstance.Show();
             }
@@ -92,6 +131,10 @@ namespace GoingOutApp
             {
                 _addWindowInstance.Focus();
             }
+        }
+        private void AddEventWindow_EventAdded(object sender, EventArgs e)
+        {
+            OnShown();
         }
     }
 }
