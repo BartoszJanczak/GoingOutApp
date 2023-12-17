697fe59973f8c3e50ab151f6bb4e06cd79aedb6c
Maciej Puchalski <maciejpe34@gmail.com>
2023-12-17 21:40:40 +0100

    Create adressinfor model used for request

---
+++ GoingOutApp/Models/AddressInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoingOutApp.Models
+{
+    public class AddressInfo
+    {
+        public string City { get; }
+        public string Street { get; }
+        public string HouseNumber { get; }
+        public string PostalCode { get; }
+
+        public AddressInfo(string city, string street, string houseNumber, string postalCode)
+        {
+            City = city;
+            Street = street;
+            HouseNumber = houseNumber;
+            PostalCode = postalCode;
+        }
+    }
+
+}
